3f5103b603e538ff7671a50612411575c6c2a208
Mateus Dias <ma.domingues.dias@gmail.com>
2020-02-28 10:26:25 -0300

    funcionando interpolações

--- interpol.py
+++ interpol.py
@@ -1,14 +1,34 @@
+import matplotlib.pyplot as plt
 import numpy as np
-from sklearn.linear_model import LinearRegression
+from scipy import interpolate
 
-X = np.array([[235000], [294000]]) # pesos
-y = np.array([0, 50]) # porcentagem
 
-reg = LinearRegression().fit(X, y)
+x = np.array([235000,255000, 294000, 298000,310000]) 
+y = np.array([0, 25, 50, 56,100]) 
 
-b = reg.intercept_
-a = reg.coef_[0] # como só tem uma variável, então seria ax+b (pega indice 0). Se tivesse mais seria a1*x1 + a2*x2 + b
+# x = np.linspace(0, 10, num=11, endpoint=True)
+# y = np.cos(-x**2/9.0)
 
-print(a, b)
 
-print(a * 3 + b) # aprox 25
+f = interpolate.interp1d(x, y)
+f3 = interpolate.interp1d(x, y, kind='cubic')
+
+# strain =input("Entre com o strain: ")
+
+xnew = np.arange(235001,309999,1)
+# xnew = np.linspace(0, 10, num=41, endpoint=True)
+ynew = f(xnew)
+ynew3= f3(xnew)
+
+print(ynew)
+
+# plt.plot(x,y,'o',xnew,ynew,'-', xnew,ynew3, '--')
+plt.plot(x,y,'o',xnew,ynew,'-')
+
+
+plt.show()
+# plt.show()
+
+# response = a * int(strain) + b
+# print(response)
+# print(a, b)
