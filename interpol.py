3fdebbbeba7fd989b83029a8a2cac3d4cb5c6213
Mateus Dias <ma.domingues.dias@gmail.com>
2020-03-02 10:51:30 -0300

    funcionando

--- interpol.py
+++ interpol.py
@@ -1,34 +1,47 @@
 import matplotlib.pyplot as plt
 import numpy as np
 from scipy import interpolate
+import pandas as p
 
 
-x = np.array([235000,255000, 294000, 298000,310000]) 
-y = np.array([0, 25, 50, 56,100]) 
+x = np.array([-100000,-65500,1500, 31400,80000]) 
+y = np.array([0,0, 7500, 15106, 16000]) 
 
-# x = np.linspace(0, 10, num=11, endpoint=True)
-# y = np.cos(-x**2/9.0)
+array_kilos = np.genfromtxt('kilos.csv',delimiter='\t')
+print(array_kilos[:,2])
 
+xx = array_kilos[:,1]
+yy = array_kilos[:,2]
 
 f = interpolate.interp1d(x, y)
-f3 = interpolate.interp1d(x, y, kind='cubic')
+# f3 = interpolate.interp1d(x, y, kind='cubic')
 
-# strain =input("Entre com o strain: ")
 
-xnew = np.arange(235001,309999,1)
+# while True:
+#     print(f(input('Digite o strain: ')))
+# print(str(f))
+# # strain =input("Entre com o strain: ")
+# print(type(f))
+# print(dir(f))
+# xnew = np.arange(235001,309999,1)
+
 # xnew = np.linspace(0, 10, num=41, endpoint=True)
-ynew = f(xnew)
-ynew3= f3(xnew)
+ynew = f(yy)
 
-print(ynew)
+# ynew3= f3(xnew)
 
 # plt.plot(x,y,'o',xnew,ynew,'-', xnew,ynew3, '--')
-plt.plot(x,y,'o',xnew,ynew,'-')
-
-
+plt.plot(x,y,'o',yy,ynew,'-')
 plt.show()
+
+plt.plot(xx,ynew,'o', xx, moving_average(ynew, n=4))
+plt.show()
+
+
+
 # plt.show()
 
 # response = a * int(strain) + b
 # print(response)
 # print(a, b)
+
